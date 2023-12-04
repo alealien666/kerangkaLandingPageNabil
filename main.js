6cb1ecab887bc2e34824348e9f7d173802aa4842
alealien666 <andraale28@gamil.com>
2023-12-05 02:29:17 +0700

    landing page

---
+++ main.js
@@ -0,0 +1,17 @@
+// scroll trigger
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray("#panel");
+const container = document.querySelector(".kontener");
+
+gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".kontener",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: () => `+=${container.offsetWidth}`,
+    },
+});
